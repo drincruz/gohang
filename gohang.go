7686fa368ed5e6cb736b1b5b4728de66bff57a08
Adrian Cruz <drincruz@gmail.com>
2020-10-03 20:07:26 -0400

    Add initial gohang
    This is technically an update from my gohang.go gist, but
    this one has different responses.

---
+++ gohang.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+func okTwoHundred(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("{ \"data\": \"200 OK\"}"))
+}
+
+func fiveHundred(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusInternalServerError)
+	w.Write([]byte("{ \"data\": \"500 Internal Server Error\"}"))
+}
+
+func notFound(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	w.Write([]byte("{ \"data\": \"404 Not Found\"}"))
+}
+
+func slowResponse(w http.ResponseWriter, r *http.Request) {
+	time.Sleep(5 * time.Second)
+	w.Write([]byte("{ \"data\": \"slow response\"}"))
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", okTwoHundred)
+	mux.HandleFunc("/500", fiveHundred)
+	mux.HandleFunc("/404", notFound)
+	mux.HandleFunc("/slow", slowResponse)
+
+	log.Println("[INFO] Now listening on :80")
+	http.ListenAndServe(":80", mux)
+}
