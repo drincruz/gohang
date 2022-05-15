b4621fb6a17bdfddacbd23e0da09a5fd139e7bdc
Adrian Cruz <drincruz@gmail.com>
2022-05-14 21:46:47 -0400

    Write a proper application/json header
    We are returning JSON body, we should also return an application/json
    header.

--- gohang.go
+++ gohang.go
@@ -8,22 +8,30 @@ import (
 
 const DEFAULT_PORT string = ":5000"
 
+func writeJsonResponseHeader(w http.ResponseWriter) {
+	w.Header().Set("Content-Type", "application/json")
+}
+
 func okTwoHundred(w http.ResponseWriter, r *http.Request) {
+	writeJsonResponseHeader(w)
 	w.Write([]byte("{ \"data\": \"200 OK\"}"))
 }
 
 func fiveHundred(w http.ResponseWriter, r *http.Request) {
+	writeJsonResponseHeader(w)
 	w.WriteHeader(http.StatusInternalServerError)
 	w.Write([]byte("{ \"data\": \"500 Internal Server Error\"}"))
 }
 
 func notFound(w http.ResponseWriter, r *http.Request) {
+	writeJsonResponseHeader(w)
 	w.WriteHeader(http.StatusNotFound)
 	w.Write([]byte("{ \"data\": \"404 Not Found\"}"))
 }
 
 func slowResponse(w http.ResponseWriter, r *http.Request) {
 	time.Sleep(5 * time.Second)
+	writeJsonResponseHeader(w)
 	w.Write([]byte("{ \"data\": \"slow response\"}"))
 }
 
