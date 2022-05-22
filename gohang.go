3ebb911623e7044da8f3b2e4ce810bacea347f24
Adrian Cruz <drincruz@gmail.com>
2022-05-21 20:19:44 -0400

    Add tests for the handlers
    Finally getting around to writing some tests... :)

--- gohang.go
+++ gohang.go
@@ -12,24 +12,24 @@ func writeJsonResponseHeader(w http.ResponseWriter) {
 	w.Header().Set("Content-Type", "application/json")
 }
 
-func okTwoHundred(w http.ResponseWriter, r *http.Request) {
+func OkTwoHundred(w http.ResponseWriter, r *http.Request) {
 	writeJsonResponseHeader(w)
-	w.Write([]byte("{ \"data\": \"200 OK\"}"))
+	w.Write([]byte("{ \"data\": \"200 OK\" }"))
 }
 
-func fiveHundred(w http.ResponseWriter, r *http.Request) {
+func FiveHundred(w http.ResponseWriter, r *http.Request) {
 	writeJsonResponseHeader(w)
 	w.WriteHeader(http.StatusInternalServerError)
-	w.Write([]byte("{ \"data\": \"500 Internal Server Error\"}"))
+	w.Write([]byte("{ \"data\": \"500 Internal Server Error\" }"))
 }
 
-func notFound(w http.ResponseWriter, r *http.Request) {
+func NotFound(w http.ResponseWriter, r *http.Request) {
 	writeJsonResponseHeader(w)
 	w.WriteHeader(http.StatusNotFound)
-	w.Write([]byte("{ \"data\": \"404 Not Found\"}"))
+	w.Write([]byte("{ \"data\": \"404 Not Found\" }"))
 }
 
-func slowResponse(w http.ResponseWriter, r *http.Request) {
+func SlowResponse(w http.ResponseWriter, r *http.Request) {
 	time.Sleep(5 * time.Second)
 	writeJsonResponseHeader(w)
 	w.Write([]byte("{ \"data\": \"slow response\"}"))
@@ -37,10 +37,10 @@ func slowResponse(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", okTwoHundred)
-	mux.HandleFunc("/500", fiveHundred)
-	mux.HandleFunc("/404", notFound)
-	mux.HandleFunc("/slow", slowResponse)
+	mux.HandleFunc("/", OkTwoHundred)
+	mux.HandleFunc("/500", FiveHundred)
+	mux.HandleFunc("/404", NotFound)
+	mux.HandleFunc("/slow", SlowResponse)
 
 	log.Printf("[INFO] Now listening on %s", DEFAULT_PORT)
 	http.ListenAndServe(DEFAULT_PORT, mux)
